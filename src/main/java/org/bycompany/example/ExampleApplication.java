40d7f7c3f9f6b3af7468d43831e3602dc08d7f74
Jabesbyanx <108432086+Jbyanx@users.noreply.github.com>
2024-05-20 15:49:26 -0500

    spring.profiles.active from code

--- src/main/java/org/bycompany/example/ExampleApplication.java
+++ src/main/java/org/bycompany/example/ExampleApplication.java
@@ -3,13 +3,16 @@ package org.bycompany.example;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Collections;
+
 
 @SpringBootApplication
 public class ExampleApplication {
 
     public static void main(String[] args) {
-
-        var ctx = SpringApplication.run(ExampleApplication.class, args); //context
+        var app = new SpringApplication(ExampleApplication.class);
+        app.setDefaultProperties(Collections.singletonMap("spring.profiles.active","test"));
+        var ctx = app.run(args); //context
 
         MyFirstService myFirstService = ctx.getBean(MyFirstService.class);
         System.out.println(myFirstService.tellAStory());
