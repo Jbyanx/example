1bdba6cebf33c1b91fd5e2f4dc4dd87f1ba46f41
Jabesbyanx <108432086+Jbyanx@users.noreply.github.com>
2024-05-26 18:57:39 -0500

    RequestParam, RequestBody & PathVariable

--- src/main/java/org/bycompany/example/FirstController.java
+++ src/main/java/org/bycompany/example/FirstController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 public class FirstController {
 
-    @GetMapping("/hello")
+    //@GetMapping("/hello")
     public String sayHello(){
         return "Hello from my First controller";
     }
@@ -24,5 +24,18 @@ public class FirstController {
     @PostMapping("/post-order-record")
     public String postOrderRecord(@RequestBody OrderRecord orderRecord    ){
         return "request accepted and order is : " + orderRecord.toString();
+    }
+
+    @GetMapping("/hello/{user-name}")
+    public String pathVar(@PathVariable("user-name") String userName){
+        return "my value = "+ userName;
+    }
+
+    //localhost:8080/hello?param_name=paramvalue&param_name2=paramvalue2
+    @GetMapping("/hello")
+    public String paramVar(
+            @RequestParam("user-name") String userName,
+            @RequestParam("last-name") String userLastName){
+        return "my value = "+ userName+ " "+userLastName;
     }
 }
