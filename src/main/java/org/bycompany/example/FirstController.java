b86f176503a7a1943cd5edc08cea427902f468ea
Jabesbyanx <108432086+Jbyanx@users.noreply.github.com>
2024-05-27 14:04:18 -0500

    ambiguous method

--- src/main/java/org/bycompany/example/FirstController.java
+++ src/main/java/org/bycompany/example/FirstController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 public class FirstController {
 
-    //@GetMapping("/hello")
+    @GetMapping("/hello")
     public String sayHello(){
         return "Hello from my First controller";
     }
